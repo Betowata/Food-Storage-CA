7ae2db3f07cddde99f1c189307d00d79ae3c017d
Betowata <147404743+Betowata@users.noreply.github.com>
2025-10-25 20:41:19 +0100

    adding the classes
    some of the description that the code will have

---
+++ FastFoodCA.java/Storage.java
@@ -0,0 +1,9 @@
+/**
+ * This one will Model the rectangular Storage Unit.
+    Using a Deque so we can add and remove items
+ * from the front or the end side.
+ */
+
+public class Storage {
+    
+}
